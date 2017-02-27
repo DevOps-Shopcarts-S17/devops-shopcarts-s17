275f9e6127bb732d82a2d75581b00bf539f87154
Hongtao Cheng <hc1817@nyu.edu>
2017-02-27 18:07:56 -0500

    GET APIS: 	/shopcarts #for list all shop carts 	/shopcarts/{sid} #for displaying all products in shopcart sid 	/shopcarts?=sid #for displaying all products in shopcart sid 	/shopcarts/{sid}/products #for displaying all products in shopcart sid 	/shopcarts/{sid}/products?sku={sku} #for displaying product sku in shopcart sid 	/shopcarts/{sid}/products/{sku} #for displaying product sku in shopcart sid

--- shopcart.py
+++ shopcart.py
@@ -75,7 +75,111 @@ shopping_carts = [
 ######################################################################
 @app.route('/')
 def index():
-    return make_response("Hello World", HTTP_200_OK)
+    shopcarts_url = request.base_url + "shopcarts"
+    return make_response(jsonify(name='Shopcart Demo REST API Service',
+                   version='1.0',
+                   url=shopcarts_url
+                   ), HTTP_200_OK)
+
+######################################################################
+# LIST ALL SHOPCARTS FOR ALL USERS
+######################################################################
+#USAGE: /shopcarts or #USAGE: /shopcarts?sid=1
+@app.route('/shopcarts', methods=['GET'])
+def list_shopcarts():
+    results=[]
+    sid=request.args.get('sid')
+    if sid:
+        #Extra check for query input
+        try:
+            sid = int(sid)
+        except ValueError:
+            return make_response(jsonify(shopping_carts), HTTP_200_OK)
+
+        results=[cart for cart in shopping_carts if cart['sid']==int(sid)]
+        if len(results)==0:
+            results=shopping_carts
+        else:
+
+            if len(results[0]['products'])==0:
+                results='The cart contains no products'
+            else:
+                results=results[0]['products']
+    else:
+        results=shopping_carts
+
+    return make_response(jsonify(results), HTTP_200_OK)
+
+######################################################################
+# RETRIEVE USER'S PRODUCTS IN THE CART
+######################################################################
+#USAGE: /shopcarts/3
+@app.route('/shopcarts/<int:sid>', methods=['GET'])
+def get_shopcart(sid):
+    carts = [cart for cart in shopping_carts if cart['sid']==sid]
+    if len(carts) > 0:
+        message = carts[0]['products']
+        if not message:
+            message='The cart contains no products'
+        rc = HTTP_200_OK
+    else:
+        message = { 'error' : 'Shopping Cart with id: %s was not found' % str(sid) }
+        rc = HTTP_404_NOT_FOUND
+
+    return make_response(jsonify(message), rc)
+
+######################################################################
+# RETRIEVE USER'S PRODUCTS IN THE CART
+######################################################################
+#USAGE: /shopcarts/3/products or /shopcarts/3/products?sku=114672050
+@app.route('/shopcarts/<int:sid>/products', methods=['GET'])
+def get_products(sid):
+    carts = [cart for cart in shopping_carts if cart['sid']==sid]
+    sku = request.args.get('sku')
+    if len(carts) > 0:
+        products = carts[0]['products']
+        if(len(products)==0):
+            message='The cart contains no products'
+        else:
+            if sku:
+                #Extra check for query input
+                try:
+                    sku = int(sku)
+                except ValueError:
+                    return make_response(jsonify(products), HTTP_200_OK)
+
+                message=[product for product in products if product['sku']==int(sku)]
+                if len(message)==0:
+                    message=products
+                else:
+                    message=message[0]
+            else:
+                message=products
+        rc = HTTP_200_OK
+    else:
+        message = { 'error' : 'Shopping Cart with id: %s was not found' % str(sid) }
+        rc = HTTP_404_NOT_FOUND
+
+    return make_response(jsonify(message), rc)
+
+######################################################################
+# RETRIEVE A PRODUCT FROM USER'S SHOPCART
+######################################################################
+#USAGE: /shopcarts/3/products/114672050
+@app.route('/shopcarts/<int:sid>/products/<int:sku>', methods=['GET'])
+def get_product(sid,sku):
+    carts = [cart for cart in shopping_carts if cart['sid']==sid]
+    if(len(carts)==0):
+        message = { 'error' : 'Shopping Cart with id: %s was not found' % str(sid) }
+        return make_response(jsonify(message),HTTP_404_NOT_FOUND)
+    else:
+        products=[product for product in carts[0]['products'] if product['sku']==sku]
+        if(len(products)==0):
+            message = { 'error' : 'Product with sku: %s was not found in the cart for user %s' % (str(sku), str(sid))}
+            return make_response(jsonify(message),HTTP_404_NOT_FOUND)
+        else:
+            return make_response(jsonify(products[0]), HTTP_200_OK)
+
 
 @app.before_first_request
 def setup_logging():
