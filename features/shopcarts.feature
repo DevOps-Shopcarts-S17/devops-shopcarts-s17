d7e28353ff21cd3d31be9a9fec1dc949c7eabfe7
Pratik Parag Karnik <pratik.karnik24@gmail.com>
2017-04-17 15:06:45 -0400

    BDD Creat Shopcarts

--- features/shopcarts.feature
+++ features/shopcarts.feature
@@ -7,3 +7,26 @@ Scenario: The server is running
     When I visit the "home page"
     Then I should see "Shopcart Demo REST API Service"
     Then I should not see "404 Not Found"
+
+Scenario: Create a new shopcart
+  When I create a new shopcart for uid "6"
+    | uid   |
+    | 6     |
+  Then I should see a new shopcart with uid "6"
+
+Scenario: Create a new shopcart
+  When I create a new shopcart for uid "1"
+    | uid   |
+    | 1     |
+  Then I should see "Shopping Cart for uid 1 already exists"
+
+Scenario: Create a new shopcart
+  When I create a new shopcart without a uid
+  Then I should see "Data is not valid"
+
+Scenario: Create a new shopcart
+  When I create a new shopcart with uid "7" having a product with sku "119873437", quantity "55", name "Lego" and unitprice "100"
+    | uid   | product sku | product quantity | product name | product unitprice |
+    | 7     | 119873437   | 55               | Lego         | 100               |
+  Then I should see a new shopcart with uid "7"
+  And I should see a product having sku "119873437", quantity "55", name "Lego" and unitprice "100"
--- features/steps/shopcart_steps.py
+++ features/steps/shopcart_steps.py
@@ -2,14 +2,55 @@ from behave import *
 import json
 import shopcart as server
 
+# Status Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
+HTTP_400_BAD_REQUEST = 400
+HTTP_404_NOT_FOUND = 404
+HTTP_409_CONFLICT = 409
+
 @when(u'I visit the "home page"')
 def step_impl(context):
     context.resp = context.app.get('/')
 
 @then(u'I should see "{message}"')
 def step_impl(context, message):
-    assert message in context.resp.data
+	assert message in context.resp.data
 
 @then(u'I should not see "{message}"')
 def step_impl(context, message):
     assert message not in context.resp.data
+
+@when(u'I create a new shopcart for uid "{uid}"')
+def step_impl(context,uid):
+	new_shopcart = { "uid": int(uid)}
+	data = json.dumps(new_shopcart)
+	context.resp = context.app.post('/shopcarts', data=data, content_type='application/json')
+
+@when(u'I create a new shopcart without a uid')
+def step_impl(context):
+	new_shopcart = {}
+	data = json.dumps(new_shopcart)
+	context.resp = context.app.post('/shopcarts', data=data, content_type='application/json')
+
+@when(u'I create a new shopcart with uid "{id}" having a product with sku "{sku}", quantity "{quantity}", name "{name}" and unitprice "{unitprice}"')
+def step_impl(context,id,sku,quantity,name,unitprice):
+	new_shopcart = { "uid": int(id), "products": [{"sku" : int(sku), "quantity" : int(quantity), "name" : name , "unitprice" : float(unitprice)}] }
+	data = json.dumps(new_shopcart)
+	context.resp = context.app.post('/shopcarts', data=data, content_type='application/json')
+
+@then(u'I should see a new shopcart with uid "{id}"')
+def step_impl(context,id):
+	new_json = json.loads(context.resp.data)
+	assert context.resp.status_code == HTTP_201_CREATED
+	assert int(new_json['uid']) == int(id)
+
+@then(u'I should see a product having sku "{sku}", quantity "{quantity}", name "{name}" and unitprice "{unitprice}"')
+def step_impl(context,sku,quantity,name,unitprice):
+	new_json = json.loads(context.resp.data)
+	for i in range(0,len(new_json['products'])):
+		if new_json['products'][0]['sku'] == int(sku):
+			assert new_json['products'][0]['quantity'] == int(quantity)
+			assert new_json['products'][0]['name'] == name
+			assert new_json['products'][0]['unitprice'] == int(unitprice)
