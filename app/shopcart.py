251b77439814e242e17be6fa0b3d1ddc9da98fff
Jiwei Xu <jx734@nyu.edu>
2017-04-27 15:22:00 -0400

    change  the shopping_carts to empty list. so now all test except the invalid one should fail.

--- app/shopcart.py
+++ app/shopcart.py
@@ -61,46 +61,7 @@ lock = Lock()
 ######################################################################
 # Data Store, pre-populated with sample data
 ######################################################################
-shopping_carts = [
-    {
-        'uid':1,
-        'sid':  1,
-        'subtotal': 0.00,
-        'products': [
-            {
-                'sku': 123456780,
-                'quantity': 2,
-                'name': "Settlers of Catan",
-                'unitprice': 27.99
-            },
-            {
-                'sku': 876543210,
-                'quantity': 1,
-                'name': "Risk",
-                'unitprice': 27.99
-            }
-        ]
-    },
-    {
-        'uid':2,
-        'sid':  2,
-        'subtotal': 0.00,
-        'products': []
-    },
-    {
-        'uid': 3,
-        'sid': 3,
-        'subtotal': 0.00,
-        'products': [
-            {
-                'sku': 114672050,
-                'quantity': 1,
-                'name': "Game of Life",
-                'unitprice': 13.99
-            }
-        ]
-    }
-]
+shopping_carts = []
 
 current_shopping_cart_id = 3
 
